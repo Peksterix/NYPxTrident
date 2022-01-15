73872c5f6130ae7ac5c966f062115eab059c7d54
Peksterix <49780425+Peksterix@users.noreply.github.com>
2022-01-16 03:51:54 +0800

    i forgot what i did

--- NYP x Trident/Assets/WaterGunTag/Scripts/GameObject/PointObject/CreatePointItem.cs
+++ NYP x Trident/Assets/WaterGunTag/Scripts/GameObject/PointObject/CreatePointItem.cs
@@ -10,13 +10,15 @@ using UnityEngine;
 
 using Mirror;
 
-public class CreatePointItem : MonoBehaviour
+public class CreatePointItem : NetworkBehaviour
 {
     //生成する物のリスト
     [SerializeField] private List<GameObjectBase> m_pointItem = new List<GameObjectBase>();
 
     //生成する物のリスト（レア）
     [SerializeField] private List<GameObjectBase> m_rarePointItem = new List<GameObjectBase>();
+
+    [SerializeField] private List<Transform> m_pointItemSpawnPoints = new List<Transform>();
 
     //生成までの時間（最速）
     [SerializeField] private float m_createTime_Fast = 5;
@@ -25,6 +27,8 @@ public class CreatePointItem : MonoBehaviour
     //レアものが生成される物の確率
     [SerializeField] private float m_createRare = 10.0f;
 
+    public GameObject m_pointObjectContainer;
+
     //制限時間
     private GameObject m_time;
 
@@ -32,6 +36,7 @@ public class CreatePointItem : MonoBehaviour
     private GameObject m_createObj;
 
     //生成までの時間計測
+    [SyncVar]
     private float m_createTime = 0.0f;
 
     //ゲームマネージャー
@@ -40,6 +45,8 @@ public class CreatePointItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (!isServer)
+            return;
         //シード値生成
         Random.InitState(System.DateTime.Now.Millisecond);
         m_createTime = m_createTime_Fast;
@@ -49,20 +56,20 @@ public class CreatePointItem : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         //制限時間が終わっていたら何もしない
         //if (m_wgtGameManager.GetComponent<WGTGameManager>().GetIsStopGame())
-        if (WGTGameManager.GetCurrGameState() == WGTGameManager.GameState.Ended)
+        if (WGTGameManager.GetCurrGameState() == WGTGameManager.GameState.Ended || !isServer)
         {
             return;
         }
 
         //子がいるなら何もしないで終わる
-        if (0<this.gameObject.transform.childCount)
-        {
-            m_createTime = m_createTime_Fast;
-            return;
-        }
+        //if (0<this.gameObject.transform.childCount)
+        //{
+        //    m_createTime = m_createTime_Fast;
+        //    return;
+        //}
 
         m_createTime-= Time.deltaTime;
 
@@ -79,15 +86,46 @@ public class CreatePointItem : MonoBehaviour
             //レアアイテム生成
             if (Random.Range(0.0f, m_createRare) <= 1.0f)
             {
-                m_createObj = Instantiate(m_rarePointItem[Random.Range(0, m_rarePointItem.Count)].gameObject, this.transform.position, Quaternion.identity);
-                m_createObj.transform.parent = this.transform;
+                SpawnRarePointItem();
             }
             else //通常アイテム生成
             {
-                m_createObj = Instantiate(m_pointItem[Random.Range(0, m_pointItem.Count)].gameObject, this.transform.position, Quaternion.identity);
-                m_createObj.transform.parent = this.transform;
+                SpawnPointItem();
             }
                
         }
+    }
+
+    [ClientRpc]
+    void SpawnPointItem()
+    {
+        Transform PointItemSpawnPoint = m_pointItemSpawnPoints[Random.Range(0, m_pointItemSpawnPoints.Count)];
+
+        if (PointItemSpawnPoint.childCount > 0)
+            return;
+
+        m_createObj = Instantiate(m_pointItem[Random.Range(0, m_pointItem.Count)].gameObject, 
+            PointItemSpawnPoint.position,
+            Quaternion.identity,
+            PointItemSpawnPoint);
+
+        NetworkServer.Spawn(m_createObj);
+    }
+
+    [ClientRpc]
+    void SpawnRarePointItem()
+    {
+        Transform RarePointItemSpawnPoint = m_pointItemSpawnPoints[Random.Range(0, m_pointItemSpawnPoints.Count)];
+
+        if (RarePointItemSpawnPoint.childCount > 0)
+            return;
+
+        //m_createObj = Instantiate(m_rarePointItem[Random.Range(0, m_rarePointItem.Count)].gameObject, this.transform.position, Quaternion.identity);
+        m_createObj = Instantiate(m_rarePointItem[Random.Range(0, m_rarePointItem.Count)].gameObject,
+           RarePointItemSpawnPoint.position,
+           Quaternion.identity,
+           RarePointItemSpawnPoint);
+
+        NetworkServer.Spawn(m_createObj);
     }
 }
